1b34693f7f3717350687f25e37cfddc942c299b8
Paul Paginton <34237608+pagyP@users.noreply.github.com>
2025-10-29 11:01:48 +0000

    Update BGP-Lab.tf

--- BGP-Lab.tf
+++ BGP-Lab.tf
@@ -21,6 +21,13 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.1.0.0/24"]
 }
 
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.bgp_lab.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
 resource "azurerm_virtual_network" "vnet2" {
   name                = "bgp-vnet2"
   address_space       = ["10.2.0.0/16"]

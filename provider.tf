d8ae47368f4d3569c0fc74088788bd5d5df8524e
Paul Paginton <34237608+pagyP@users.noreply.github.com>
2025-10-29 09:39:36 +0000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+
+}
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
